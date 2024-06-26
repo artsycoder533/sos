f2b87acd2d85c6fa9ebd8039ef6d31ce8e5feae1
Natasha Johnson <njohnson533@gmail.com>
2024-06-25 22:40:48 -0400

    chore: add list your business page

---
+++ src/utils/constants.ts
@@ -0,0 +1,36 @@
+export const STANDARD_MONTHLY_PRICE = 6.99
+export const STANDARD_YEARLY_PRICE = 74.99
+export const PREMIUM_MONTHLY_PRICE = 10.99
+export const PREMIUM_YEARLY_PRICE = 119.99
+
+export const STANDARD_FEATURES = [
+    "Business Name",
+    "Address",
+    "Email Address",
+    "Phone Number",
+    "Website Link",
+    "Business Hours",
+    "Social Media Links",
+    "Booking Site Link",
+    "Services List",
+    "Business Description",
+    "Business Logo or Profile Image",
+]
+
+export const PREMIUM_FEATURES = [
+    "Business Name",
+    "Address",
+    "Email Address",
+    "Phone Number",
+    "Website Link",
+    "Business Hours",
+    "Social Media Links",
+    "Booking Site Link",
+    "Services List",
+    "Business Description",
+    "Business Logo or Profile Image",
+    "Image Gallery w/ up to 6 images",
+    "Listed in Featured Businesses section",
+    "FAQ Section",
+    "Sharable QR Code"
+]
